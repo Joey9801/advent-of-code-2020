e2fa767e05e73cb04db6460cb9f699b71b4b12fd
Joe Roberts <josephroberts11@gmail.com>
2020-12-01 23:37:28 +0000

    Include unit tests for day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,14 +1,25 @@
-use std::fs::File;
-use std::io::{self, BufReader, BufRead};
-
-fn main() -> Result<(), io::Error> {
-    let file = File::open("./day_1/input.txt")?;
-    let reader = BufReader::new(file);
-    let numbers = reader.lines()
-        .map(|l| l.expect("Failed to read line"))
+fn input() -> Vec<u64> {
+    let input = include_str!("../input.txt");
+    input.lines()
         .map(|l| l.trim().parse().expect(&format!("{} wasn't a valid u64", l)))
-        .collect::<Vec<u64>>();
+        .collect::<Vec<u64>>()
+}
+
+fn part_1(numbers: &[u64]) -> Option<u64> {
+    for i in 0..(numbers.len() - 2) {
+        for j in (i+1)..(numbers.len() - 1) {
+            let x = numbers[i];
+            let y = numbers[j];
+            if x + y == 2020 {
+                return Some(x * y);
+            }
+        }
+    }
     
+    None
+}
+
+fn part_2(numbers: &[u64]) -> Option<u64> {
     for i in 0..(numbers.len() - 2) {
         for j in (i+1)..(numbers.len() - 1) {
             for k in (j+1)..numbers.len() {
@@ -16,11 +27,29 @@ fn main() -> Result<(), io::Error> {
                 let y = numbers[j];
                 let z = numbers[k];
                 if x + y + z == 2020 {
-                    println!("{} + {} + {} = 2020, {} * {} * {} = {}", x, y, z, x, y, z, x * y * z);
+                    return Some(x * y * z);
                 }
             }
         }
     }
     
-    Ok(())
+    None
+}
+
+fn main() {
+    let input = input();
+    println!("part 1 => {}", part_1(&input).unwrap());
+    println!("part 2 => {}", part_2(&input).unwrap());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_day_1() {
+        let input = input();
+        assert_eq!(part_1(&input).expect("Found no solution to part 1"), 877971);
+        assert_eq!(part_2(&input).expect("Found no solution to part 2"), 203481432);
+    }
 }
