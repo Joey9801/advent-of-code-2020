e1bc7501a1f0f0f29719cecb74a034c0d1050690
Joe Roberts <josephroberts11@gmail.com>
2020-12-02 18:41:18 +0000

    Day 2, part 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,6 +1,7 @@
 const INPUT: &str = include_str!("../input.txt");
 
-fn is_valid(line: &str) -> bool {
+fn is_valid_1(line: &str) -> bool {
+    debug_assert!(line.is_ascii());
     let dash = line.find('-').unwrap();
     let space = line.find(' ').unwrap();
     let colon = line.find(':').unwrap();
@@ -17,10 +18,25 @@ fn is_valid(line: &str) -> bool {
     (min..(max + 1)).contains(&count)
 }
 
+fn is_valid_2(line: &str) -> bool {
+    debug_assert!(line.is_ascii());
+    let dash = line.find('-').unwrap();
+    let space = line.find(' ').unwrap();
+    let colon = line.find(':').unwrap();
+    
+    let idx_1: usize = line[0..dash].parse().unwrap();
+    let idx_2: usize = line[(dash + 1)..space].parse().unwrap();
+    let letter = line[(space+1)..(space+2)].chars().next().unwrap();
+    let password = &line[(colon + 2)..];
+    
+    let get_char = |idx| password.chars().skip(idx - 1).next();
+    (get_char(idx_1) == Some(letter)) ^ (get_char(idx_2) == Some(letter))
+}
+
 fn main() {
     let valid_count = INPUT
         .lines()
-        .filter(|l| is_valid(*l))
+        .filter(|l| is_valid_2(*l))
         .count();
 
     println!("There are {} valid passwords", valid_count);
@@ -31,9 +47,34 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_day_2_checker() {
-        assert!(is_valid("1-3 a: abcde"));
-        assert!(!is_valid("1-3 b: cdefg"));
-        assert!(is_valid("2-9 c: ccccccccc"));
+    fn test_day_2_is_valid_1() {
+        assert!(is_valid_1("1-3 a: abcde"));
+        assert!(!is_valid_1("1-3 b: cdefg"));
+        assert!(is_valid_1("2-9 c: ccccccccc"));
+    }
+    
+    #[test]
+    fn test_day_2_is_valid_2() {
+        assert!(is_valid_2("1-3 a: abcde"));
+        assert!(!is_valid_2("1-3 b: cdefg"));
+        assert!(!is_valid_2("2-9 c: ccccccccc"));
+    }
+    
+    #[test]
+    fn test_day_2_part_1() {
+        let valid_count = INPUT
+            .lines()
+            .filter(|l| is_valid_1(*l))
+            .count();
+        assert_eq!(valid_count, 524);
+    }
+    
+    #[test]
+    fn test_day_2_part_2() {
+        let valid_count = INPUT
+            .lines()
+            .filter(|l| is_valid_2(*l))
+            .count();
+        assert_eq!(valid_count, 485)
     }
 }
