300fb82820da5e6594bfaaeb211f997283aeef94
Joe Roberts <josephroberts11@gmail.com>
2020-12-03 12:16:32 +0000

    Day 3, part 1

---
+++ day_3/src/main.rs
@@ -0,0 +1,64 @@
+const INPUT: &str = include_str!("../input.txt");
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Cell {
+    Open,
+    Tree,
+}
+
+impl Cell {
+    fn from_byte(c: u8) -> Option<Self> {
+        match c {
+            b'.' => Some(Self::Open),
+            b'#' => Some(Self::Tree),
+            _ => None,
+        }
+    }
+}
+
+struct Map {
+    data: Vec<Cell>,
+    height: usize,
+    width: usize,
+}
+
+impl Map {
+    fn from_input(input: &str) -> Self {
+        debug_assert!(input.is_ascii());
+
+        let width = input.as_bytes().iter().position(|b| *b == b'\n').unwrap();
+        let data = input.as_bytes()
+            .iter()
+            .filter_map(|b| Cell::from_byte(*b))
+            .collect::<Vec<Cell>>();
+        
+        debug_assert!(data.len() % width == 0);
+        let height = data.len() / width;
+
+        Self {
+            data,
+            width,
+            height,
+        }
+    }
+    
+    fn get_cell(&self, x: usize, y: usize) -> Cell {
+        let x = x % self.width;
+        
+        self.data[y * self.width + x]
+    }
+    
+    fn count_trees(&self, step_x: usize, step_y: usize) -> usize {
+        (0..self.height)
+            .step_by(step_y)
+            .enumerate()
+            .map(|(xi, y)| self.get_cell(xi * step_x, y))
+            .filter(|cell| *cell == Cell::Tree)
+            .count()
+    }
+}
+
+fn main() {
+    let map = Map::from_input(INPUT);
+    println!("There were {} trees on the 3/1 slope", map.count_trees(3, 1));
+}
