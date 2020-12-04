71a9f7c0a4ac5222481bacbc3d38822818019543
Joe Roberts <josephroberts11@gmail.com>
2020-12-04 11:37:44 +0000

    Day 4, part 2

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,10 +1,37 @@
+use std::str::FromStr;
+
 const INPUT: &str = include_str!("../input.txt");
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Height {
+    Inches(usize),
+    Centimeter(usize),
+}
+
+impl FromStr for Height {
+    type Err = PassportParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if !s.is_ascii() {
+            Err(PassportParseError::NotAscii)
+        } else {
+            let split = s.len() - 2;
+            match &s[split..] {
+                "cm" => Ok(Self::Centimeter(s[..split].parse()?)),
+                "in" => Ok(Self::Inches(s[..split].parse()?)),
+                _ => Err(PassportParseError::ParseHeightError)
+            }
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum PassportParseError {
+    NotAscii,
     MissingField,
     UnknownField,
     ParseIntError,
+    ParseHeightError,
 }
 
 impl From<std::num::ParseIntError> for PassportParseError {
@@ -17,7 +44,7 @@ struct PassportBuilder<'a> {
     birth_year: Option<usize>,
     issue_year: Option<usize>,
     expiration_year: Option<usize>,
-    height: Option<&'a str>,
+    height: Option<Height>,
     hair_color: Option<&'a str>,
     eye_color: Option<&'a str>,
     passport_id: Option<&'a str>,
@@ -72,7 +99,7 @@ struct Passport<'a> {
     birth_year: usize,
     issue_year: usize,
     expiration_year: usize,
-    height: &'a str,
+    height: Height,
     hair_color: &'a str,
     eye_color: &'a str,
     passport_id: &'a str,
@@ -82,7 +109,9 @@ struct Passport<'a> {
 impl<'a> Passport<'a> {
     /// Parses a single passport string, returning None if invalid
     pub fn parse(s: &'a str) -> Result<Self, PassportParseError> {
-        assert!(s.is_ascii());
+        if !s.is_ascii() {
+            return Err(PassportParseError::NotAscii);
+        }
 
         let mut builder = PassportBuilder::new();
         for part in s.split_whitespace() {
@@ -94,7 +123,7 @@ impl<'a> Passport<'a> {
                 "byr" => builder.birth_year = Some(value.parse()?),
                 "iyr" => builder.issue_year = Some(value.parse()?),
                 "eyr" => builder.expiration_year = Some(value.parse()?),
-                "hgt" => builder.height = Some(value),
+                "hgt" => builder.height = Some(value.parse()?),
                 "hcl" => builder.hair_color = Some(value),
                 "ecl" => builder.eye_color = Some(value),
                 "pid" => builder.passport_id = Some(value),
@@ -105,12 +134,43 @@ impl<'a> Passport<'a> {
         
         builder.build()
     }
+    
+    pub fn is_valid(&self) -> bool {
+        fn is_hex_color(c: &str) -> bool {
+            debug_assert!(c.is_ascii());
+            let c = c.as_bytes();
+            let is_hex_char = |b: &u8|
+                (*b >= b'0' && *b <= b'9') ||
+                (*b >= b'a' && *b <= b'f');
+            
+            c[0] == b'#' && c[1..].iter().all(is_hex_char)
+        }
+
+        fn is_eye_color(c: &str) -> bool {
+            match c {
+                "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" => true,
+                _ => false,
+            }
+        }
+
+        self.birth_year >= 1920 && self.birth_year <= 2002 &&
+        self.issue_year >= 2010 && self.issue_year <= 2020 &&
+        self.expiration_year >= 2020 && self.expiration_year <= 2030 &&
+        match self.height {
+            Height::Inches(inches) => inches >= 59 && inches <= 76,
+            Height::Centimeter(cm) => cm >= 150 && cm <= 193,
+        } &&
+        is_hex_color(self.hair_color) &&
+        is_eye_color(self.eye_color) &&
+        self.passport_id.len() == 9 && self.passport_id.chars().all(|c| c.is_ascii_digit())
+    }
 }
 
 fn main() {
     let valid_passport_count = INPUT.split("\n\n")
         .map(Passport::parse)
-        .filter(Result::is_ok)
+        .filter_map(Result::ok)
+        .filter(Passport::is_valid)
         .count();
     
     println!("There were {} valid passport definitions", valid_passport_count);
@@ -134,7 +194,7 @@ mod tests {
             birth_year: 1937,
             issue_year: 2017,
             country_id: Some("147"),
-            height: "183cm",
+            height: Height::Centimeter(183),
         }));
     }
 
@@ -163,7 +223,7 @@ mod tests {
             birth_year: 1931,
             issue_year: 2013,
             country_id: None,
-            height: "179cm",
+            height: Height::Centimeter(179),
         }));
     }
 }
